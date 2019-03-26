85b49e595718f6fcc1b276821df7b99cb1f7ca5e
James Kong <james.kong@nixplay.com>
2019-03-26 16:46:17 +0800

    renamed file

--- ios/RNVideoTrimmer.m
+++ ios/RNVideoTrimmer.m
@@ -4,7 +4,7 @@
 #import <AVFoundation/AVFoundation.h>
 #import <Photos/Photos.h>
 #import <React/RCTUtils.h>
-#import "VideoTrimerViewController.h"
+#import "VideoTrimmerViewController.h"
 @import Photos;
 @interface RNVideoTrimmer () <VideoTrimerViewDelegate>
 
@@ -44,8 +44,8 @@ RCT_EXPORT_METHOD(showVideoTrimmer:(NSDictionary *)options callback:(RCTResponse
     }
 
 
-    NSBundle *podBundle = [NSBundle bundleForClass:[VideoTrimerViewController class]];
-    VideoTrimerViewController *vc;
+    NSBundle *podBundle = [NSBundle bundleForClass:[VideoTrimmerViewController class]];
+    VideoTrimmerViewController *vc;
     NSBundle *bundle;
     id data = [podBundle URLForResource:@"RNVideoTrimmer" withExtension:@"bundle"];
     if(data != nil) {
@@ -53,7 +53,7 @@ RCT_EXPORT_METHOD(showVideoTrimmer:(NSDictionary *)options callback:(RCTResponse
     } else {
         bundle = podBundle;
     }
-    vc = [[VideoTrimerViewController alloc]initWithNibName:@"VideoTrimerViewController" bundle:bundle];
+    vc = [[VideoTrimmerViewController alloc]initWithNibName:@"VideoTrimmerViewController" bundle:bundle];
     [vc setDelegate:self];
 	[vc setMaxLength: maxLength];
     [vc setMinLength: minLength];
@@ -77,22 +77,22 @@ RCT_EXPORT_METHOD(showVideoTrimmer:(NSDictionary *)options callback:(RCTResponse
 }
 
 #pragma VideoTrimerViewDelegate
-- (void)videoTrimerViewController:(nonnull VideoTrimerViewController *)videoTrimmerController didChangeStartTime:(Float64)startTime endTime:(Float64)endTime{
-    NSLog(@"videoTrimerViewController didChangeStartTime %f %f", startTime, endTime);
+- (void)VideoTrimmerViewController:(nonnull VideoTrimmerViewController *)videoTrimmerController didChangeStartTime:(Float64)startTime endTime:(Float64)endTime{
+    NSLog(@"VideoTrimmerViewController didChangeStartTime %f %f", startTime, endTime);
 }
-- (void)didFinishVideoTrimerViewController:(nonnull VideoTrimerViewController *)videoTrimmerController withStartTime:(Float64)startTime endTime:(Float64)endTime{
-    NSLog(@"didFinishVideoTrimerViewController withStartTime endTime %f %f", startTime, endTime);
+- (void)didFinishVideoTrimmerViewController:(nonnull VideoTrimmerViewController *)videoTrimmerController withStartTime:(Float64)startTime endTime:(Float64)endTime{
+    NSLog(@"didFinishVideoTrimmerViewController withStartTime endTime %f %f", startTime, endTime);
     self.callback(@[@{
                         @"uri":self.options[@"uri"],
                         @"startTime":@(startTime),
                         @"endTime":@(endTime)
                         }]);
 }
-- (void)didFinishVideoTrimerViewController:(nonnull VideoTrimerViewController *)videoTrimmerController{
-    NSLog(@"didFinishVideoTrimerViewController");
+- (void)didFinishVideoTrimmerViewController:(nonnull VideoTrimmerViewController *)videoTrimmerController{
+    NSLog(@"didFinishVideoTrimmerViewController");
     self.callback(@[@{@"error": @"user cancel"}]);
 }
-- (void)videoTrimerViewController:(nonnull VideoTrimerViewController *)videoTrimmerController didFailedWithError:(NSError *)error{
+- (void)VideoTrimmerViewController:(nonnull VideoTrimmerViewController *)videoTrimmerController didFailedWithError:(NSError *)error{
     self.callback(@[@{@"error": error.localizedDescription}]);
 }
 @end
